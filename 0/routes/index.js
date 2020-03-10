788e794895a3676352194b0a17881689baabdbe1
Awienberg <a.wienberg@hotmail.com>
2020-03-10 17:08:36 +0100

    first

---
+++ 0/routes/index.js
@@ -0,0 +1,37 @@
+const express = require('express');
+const router = express.Router();
+const modContinent = require("../models/handleContinents");
+const modCountry = require("../models/handleCountries");
+const modCities = require("../models/handleCities");
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+    res.render('index', {
+        title: 'Fragments of the World',
+        subtitle: 'Playing with the World'
+    });
+});
+
+router.get('/worldview', async function(req, res, next) {
+    res.render('worldview', {
+        title: 'Fragments of the World',
+        subtitle: 'Start by Choosing a Continent'
+    });
+});
+
+router.get('/continents', async function(req, res, next) {
+    let continents = await modContinent.getContinents({}, {sort: {name: 1}});
+    res.json(continents);
+});
+
+router.get('/countries/:cont', async function(req, res, next) {
+    let countries = await modCountry.getCountries({continent: req.params.cont}, {sort: {name: 1}});
+    res.json(countries);
+});
+
+router.get('/cities/:city', async function(req, res, next) {
+    let cities = await modCities.getCities({countrycode: req.params.city}, {sort: {name: 1}});
+    res.json(cities);
+});
+
+module.exports = router;
